ab3a2f592d3f3ebbfe7fd6b4b499446952d229d4
Marvin M <marvin23m@gmail.com>
2016-06-01 11:22:39 +0100

    Write spec for how many tasks are remaining

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,10 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Project do 
-  describe 'initialization' do
-    let(:project) { Project.new }
-    let(:task) { Task.new }
+  let(:project) { Project.new }
+  let(:task) { Task.new }
 
+  describe 'initialization' do
     it 'considers a project with no tasks to be done' do 
       expect(project).to be_done
     end
@@ -14,4 +14,34 @@ RSpec.describe Project do
       expect(project).not_to be_done
     end
   end
+
+  it 'marks a project done if its tasks are done' do 
+    project.tasks << task
+    task.mark_completed
+
+    expect(project).to be_done
+  end
+
+  describe 'estimates' do 
+    let(:done) { Task.new(size: 2, completed: true) }
+
+    let(:small_not_done) { Task.new(size: 1) }
+    let(:large_not_done) { Task.new(size: 4) }
+
+    before(:example) do 
+      project.tasks = [done, small_not_done, large_not_done]
+    end
+
+    it 'can calculate total size' do 
+      expect(project.total_size).to eq(7)
+    end 
+
+    it 'can calculate the remaining size' do 
+      expect(project.remaining_size).to eq(5)
+    end
+
+  end
 end
+
+
+
